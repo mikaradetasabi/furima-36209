0f9c2436a95801907fc3e257bd522e68de44681a
Gajugabu <86964853+mikaradetasabi@users.noreply.github.com>
2021-08-27 11:58:16 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,16 +5,16 @@ class UsersController < ApplicationController
   end
 
   def update
-    if current_user.update(user_params) # 更新出来たかを条件分岐する
-      redirect_to root_path # 更新できたらrootパスへ
+    if current_user.update(user_params)
+      redirect_to root_path
     else
-      redirect_to action: "show" # 失敗すれば再度マイページへ
+      redirect_to action: "show"
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:nickname, :email) # 編集出来る情報を制限
+    params.require(:user).permit(:nickname, :email)
   end
 end
