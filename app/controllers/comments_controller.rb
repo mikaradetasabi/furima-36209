c5539438daee3a58e6f5f0a3bf4886a74950171f
Gajugabu <86964853+mikaradetasabi@users.noreply.github.com>
2021-08-27 12:49:09 +0900

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,6 @@ end
 
 private
 def comment_params
-  params.require(:comment).permit(;text).merge(user_id: current_user.id, item_id: params[:item_id])
+  params.require(:comment).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
 end
 end
